8acfe19a14484c5b4ddbfb2a60df19e671a87d0b
Marcelo Cezário <marcelocezario@gmail.com>
2018-04-26 19:55:04 -0300

    ConsoleView

--- ProducaoAlimentos/ProducaoAlimentos/Properties/AssemblyInfo.cs
+++ ProducaoAlimentos/ProducaoAlimentos/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associada a um assembly.
-[assembly: AssemblyTitle("ProducaoAlimentos_Console")]
+[assembly: AssemblyTitle("ProducaoAlimentos")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ProducaoAlimentos_Console")]
+[assembly: AssemblyProduct("ProducaoAlimentos")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
-[assembly: Guid("12db0836-19a9-41f3-8357-8fb139a57343")]
+[assembly: Guid("78aba9e8-84f2-4f4b-9971-60f618b7be55")]
 
 // As informações da versão de um assembly consistem nos quatro valores a seguir:
 //
