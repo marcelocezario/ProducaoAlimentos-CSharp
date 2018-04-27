d138240732f1e52c2e7c6cac65c043fa70d930e3
Marcelo Cezário <marcelocezario@gmail.com>
2018-04-27 00:23:23 -0300

    Contexo : DbContext - using System.Data.Entity;

--- ProducaoAlimentos/Model/DAL/Contexto.cs
+++ ProducaoAlimentos/Model/DAL/Contexto.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Model.DAL
 {
-    class Contexto
+    class Contexto : DbContext
     {
     }
 }
