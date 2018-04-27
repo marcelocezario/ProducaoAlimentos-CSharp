33cbe589d5be26e0053d44dfc02f009676c892f2
Marcelo Cezário <marcelocezario@gmail.com>
2018-04-26 21:27:50 -0300

    Criando classe Insumo

---
+++ ProducaoAlimentos/Model/Insumo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Insumo
+    {
+        public int InsumoID { get; set; }
+        public string NomeInsumo { get; set; }
+        public string UnidadeDeMedida { get; set; }
+    }
+}
