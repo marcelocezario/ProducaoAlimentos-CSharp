0b5d8efcb0814b76e7496d0d12ee6b916c7c6906
Marcelo Cezário <marcelocezario@gmail.com>
2018-04-26 23:25:05 -0300

    Criando ItemComposiçãoProduto

---
+++ ProducaoAlimentos/Model/ItemComposicaoProduto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class ItemComposicaoProduto
+    {
+        public int ItemComposicaoProdutoID { get; set; }
+        public int ProdutoID { get; set; }
+        public int InsumoID { get; set; }
+        public virtual int _Insumo { get; set; }
+        public double QuantidadeInsumo { get; set; }
+    }
+}
