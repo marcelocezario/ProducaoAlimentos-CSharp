6afa910b28183096925e67a0bb5c2c2519080776
Marcelo Cezário <marcelocezario@gmail.com>
2018-04-30 01:49:18 -0300

    Criando ProdutoController e acertar listagem de insumos em ordem alfabetica

--- ProducaoAlimentos/Controller/InsumoController.cs
+++ ProducaoAlimentos/Controller/InsumoController.cs
@@ -9,7 +9,7 @@ namespace Controller
 {
     public class InsumoController
     {
-        public bool SalvarInsumo (Insumo insumo)
+        public bool SalvarInsumo(Insumo insumo)
         {
             ContextoSingleton.Instancia.Insumos.Add(insumo);
             ContextoSingleton.Instancia.SaveChanges();
@@ -17,12 +17,12 @@ namespace Controller
             return true;
         }
 
-        public Insumo PesquisarInsumoPorID (int insumoID)
+        public Insumo PesquisarInsumoPorID(int insumoID)
         {
             return ContextoSingleton.Instancia.Insumos.Find(insumoID);
         }
 
-        public bool ExcluirInsumo (Insumo insumo)
+        public bool ExcluirInsumo(Insumo insumo)
         {
             ContextoSingleton.Instancia.Entry(insumo).State =
                 System.Data.Entity.EntityState.Deleted;
@@ -32,7 +32,7 @@ namespace Controller
             return true;
         }
 
-        public Insumo PesquisarInsumoPorNome (string nomeInsumo)
+        public Insumo PesquisarInsumoPorNome(string nomeInsumo)
         {
             var i = from x in ContextoSingleton.Instancia.Insumos
                     where x.Nome.ToLower().Contains(nomeInsumo.Trim().ToLower())
@@ -44,6 +44,18 @@ namespace Controller
                 return null;
         }
 
-        
+        public List<Insumo> ListarInsumos()
+        {
+            return ContextoSingleton.Instancia.Insumos.ToList();
+        }
+
+        public List<Insumo> ListarInsumosOrdemAlfabetica()
+        {
+            var i = from x in ListarInsumos()
+                    orderby x.Nome
+                    select x;
+
+            return i.ToList();
+        }
     }
 }
---
+++ ProducaoAlimentos/Controller/ProdutoController.cs
@@ -0,0 +1,40 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    public class ProdutoController
+    {
+        public bool SalvarProduto(Produto produto)
+        {
+            ContextoSingleton.Instancia.Produtos.Add(produto);
+            ContextoSingleton.Instancia.SaveChanges();
+
+            return true;
+        }
+
+        public Produto PesquisarProdutoPorID(int produtoID)
+        {
+            return ContextoSingleton.Instancia.Produtos.Find(produtoID);
+        }
+
+        public bool ExcluirProduto(Produto produto)
+        {
+            ContextoSingleton.Instancia.Entry(produto).State =
+                System.Data.Entity.EntityState.Deleted;
+
+            ContextoSingleton.Instancia.SaveChanges();
+
+            return true;
+        }
+
+        public void PesquisarProdutoPorNome(string nomeProduto)
+        {
+
+        }
+    }
+}
