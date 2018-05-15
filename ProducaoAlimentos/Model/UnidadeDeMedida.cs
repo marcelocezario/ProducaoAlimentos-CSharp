2797321b9bd22f31becf25e13e0315a9beeaa743
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2018-05-15 16:40:10 -0300

    Tentando inicilizar tabela Unidades de Medida com dados já preenchidos

--- ProducaoAlimentos/Model/UnidadeDeMedida.cs
+++ ProducaoAlimentos/Model/UnidadeDeMedida.cs
@@ -8,9 +8,18 @@ namespace Model
 {
     public class UnidadeDeMedida
     {
-        public int UnidadeDeMedidaID { get }
+        public int UnidadeDeMedidaID { get; set; }
         public string Nome { get; set; }
         public string Sigla { get; set; }
         public bool Fracionavel { get; set; }
+
+
+        List <UnidadeDeMedida> unidadesDeMedida = new List<UnidadeDeMedida>()
+        {
+            new UnidadeDeMedida(){ Nome = "Centimetro", Sigla = "cm", Fracionavel = true},
+            new UnidadeDeMedida(){ Nome = "Litro", Sigla = "l", Fracionavel = true},
+            new UnidadeDeMedida(){ Nome = "Quilo", Sigla = "kg", Fracionavel = true},
+            new UnidadeDeMedida(){ Nome = "Unidade", Sigla = "un", Fracionavel = false}
+        };
     }
 }
