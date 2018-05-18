ed09e6228a78d848b721f98a0bfdbcf644a2a166
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2018-05-18 02:13:57 -0300

    Tentando acertar inicialização de dados

--- ProducaoAlimentos/Model/Insumo.cs
+++ ProducaoAlimentos/Model/Insumo.cs
@@ -13,6 +13,5 @@ namespace Model
         public string UnidadeDeMedida { get; set; }
         public double ValorTotalEstoque { get; set; }
         public double QuantidadeTotalEstoque { get; set; }
-        public double QuantidadeTotal { get; set; }
     }
 }
--- ProducaoAlimentos/Model/UnidadeDeMedida.cs
+++ ProducaoAlimentos/Model/UnidadeDeMedida.cs
@@ -12,14 +12,5 @@ namespace Model
         public string Nome { get; set; }
         public string Sigla { get; set; }
         public bool Fracionavel { get; set; }
-
-
-        List <UnidadeDeMedida> unidadesDeMedida = new List<UnidadeDeMedida>()
-        {
-            new UnidadeDeMedida(){ Nome = "Centimetro", Sigla = "cm", Fracionavel = true},
-            new UnidadeDeMedida(){ Nome = "Litro", Sigla = "l", Fracionavel = true},
-            new UnidadeDeMedida(){ Nome = "Quilo", Sigla = "kg", Fracionavel = true},
-            new UnidadeDeMedida(){ Nome = "Unidade", Sigla = "un", Fracionavel = false}
-        };
     }
 }
