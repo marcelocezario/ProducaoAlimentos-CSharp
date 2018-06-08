e10aeb6dd31d1ad24463d6c64ba8b82b7c34162c
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2018-06-08 05:13:19 -0300

    Criando Model LoteInsumo

---
+++ ProducaoAlimentos/Model/LoteInsumo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class LoteInsumo
+    {
+        public int LoteInsumoID { get; set; }
+        public int InsumoID { get; set; }
+        public virtual Insumo _Insumo { get; set; }
+        public double Qtde { get; set; }
+        public double ValorCustoMedio { get; set; }
+        public DateTime Validade { get; set; }
+        public DateTime DataCompra { get; set; }
+
+
+
+        //VERIFICAR PARA COLOCAR OPÇÃO DE MARCA DO INSUMO
+    }
+}
