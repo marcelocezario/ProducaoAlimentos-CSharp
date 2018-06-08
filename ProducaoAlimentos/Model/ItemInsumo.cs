37d5bf071917e11b977802474e4d957ae78e9ad8
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2018-06-08 05:07:58 -0300

    Criando Model ItemInsumo

---
+++ ProducaoAlimentos/Model/ItemInsumo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class ItemInsumo
+    {
+        public int ItemInsumoID { get; set; }
+        public int InsumoID { get; set; }
+        public virtual Insumo _Insumo { get; set; }
+        public double QtdeTotalEstoque { get; set; }
+        public double CustoTotalEstoque { get; set; }
+    }
+}
