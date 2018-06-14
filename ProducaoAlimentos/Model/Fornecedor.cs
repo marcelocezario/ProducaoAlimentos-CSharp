9fce3b32cfa49729975939ee68af01a6d832c5bb
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2018-06-14 07:03:53 -0300

    Criando model Fornecedor

---
+++ ProducaoAlimentos/Model/Fornecedor.cs
@@ -0,0 +1,11 @@
+﻿namespace Model
+{
+    public class Fornecedor
+    {
+        public int FornecedorID { get; set; }
+        public string Nome { get; set; }
+        public string Cpf_Cnpj { get; set; }
+
+
+    }
+}
