c8e82f6a4a70892815d6616940c54e05fa9d0c0b
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2018-06-14 07:09:17 -0300

    Incluindo Endereco, Telefone e Email na model Fornecedor

--- ProducaoAlimentos/Model/Fornecedor.cs
+++ ProducaoAlimentos/Model/Fornecedor.cs
@@ -1,4 +1,6 @@
-﻿namespace Model
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Model
 {
     public class Fornecedor
     {
@@ -6,6 +8,11 @@
         public string Nome { get; set; }
         public string Cpf_Cnpj { get; set; }
 
+        [ForeignKey("_Endereco")]
+        public int EnderecoID { get; set; }
+        public virtual Endereco _Endereco { get; set; }
 
+        public string Telefone { get; set; }
+        public string Email { get; set; }
     }
 }
