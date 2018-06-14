8966751c9f692926ec9b50da933d7a145b3cdaae
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2018-06-14 07:07:17 -0300

    Criando model Endereco

---
+++ ProducaoAlimentos/Model/Endereco.cs
@@ -0,0 +1,12 @@
+﻿namespace Model
+{
+    public class Endereco
+    {
+        public int EnderecoID { get; set; }
+        public string Logradouro { get; set; }
+        public int Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+    }
+}
