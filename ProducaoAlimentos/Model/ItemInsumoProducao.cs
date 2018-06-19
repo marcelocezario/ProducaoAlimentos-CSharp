88ce12d6af9b8812f71a265c9d84cf179b745811
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2018-06-19 19:54:53 -0300

    Criando Model ItemInsumoProducao

---
+++ ProducaoAlimentos/Model/ItemInsumoProducao.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Model
+{
+    public class ItemInsumoProducao
+    {
+        public int ItemInsumoProducaoID { get; set; }
+
+        public double QtdeInsumo { get; set; }
+        public double CustoInsumo { get; set; }
+
+        [ForeignKey("_LoteInsumo")]
+        public int LoteInsumoID { get; set; }
+        public virtual LoteInsumo _LoteInsumo { get; set; }
+
+        [ForeignKey("_LoteProduto")]
+        public int LoteProdutoID { get; set; }
+        public virtual LoteProduto _LoteProduto { get; set; }
+
+    }
+}
