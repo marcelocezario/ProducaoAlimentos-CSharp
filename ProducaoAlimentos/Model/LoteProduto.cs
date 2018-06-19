b5524aff86a9e6c3deb26a6353bf9a4295408efd
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2018-06-19 20:00:16 -0300

    Incluindo atributos em LoteProduto

--- ProducaoAlimentos/Model/LoteProduto.cs
+++ ProducaoAlimentos/Model/LoteProduto.cs
@@ -11,10 +11,12 @@ namespace Model
         public int ProdutoID { get; set; }
         public virtual Produto _Produto { get; set; }
 
+        [ForeignKey("_ItemInsumoProducao")]
         public int ItemInsumoProducaoID { get; set; }
         public virtual ItemInsumoProducao _ItemInsumoProducao { get; set; }
 
-        public double Qtde { get; set; }
+        public double QtdeInicial { get; set; }
+        public double QtdeDisponivel { get; set; }
         public double ValorCustoMedio { get; set; }
         public double ValorVendaMedio { get; set; }
         public DateTime Validade { get; set; }
